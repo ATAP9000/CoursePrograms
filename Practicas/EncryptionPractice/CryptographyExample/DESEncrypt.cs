d33de2fc10b1b7b9070319bf0816f5c88939ae76
ATAP9000 <83297229+ATAP9000@users.noreply.github.com>
2022-11-28 07:58:48 -0400

    Add files via upload

---
+++ Practicas/EncryptionPractice/CryptographyExample/DESEncrypt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyExample
+{
+    public class DESEncrypt
+    {
+        public static TripleDES CreateDES(string password)
+        {
+            MD5 mD5 = new MD5CryptoServiceProvider();
+            TripleDES dES = new TripleDESCryptoServiceProvider();
+            dES.Key = mD5.ComputeHash(Encoding.Unicode.GetBytes(password));
+            dES.IV = new byte[dES.BlockSize / 8];
+            return dES;
+        }
+
+        public string Encrypt(string text, string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            MemoryStream memoryStream = new MemoryStream();
+            TripleDES dES = CreateDES(password);
+            CryptoStream stream = new CryptoStream(memoryStream, dES.CreateEncryptor(), CryptoStreamMode.Write);
+            stream.Write(bytes, 0, bytes.Length);
+            stream.FlushFinalBlock();
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        public string Decrypt(string encryptedText, string password)
+        {
+            byte[] bytes = Convert.FromBase64String(encryptedText);
+            MemoryStream stream = new MemoryStream();
+            TripleDES dES = CreateDES(password);
+            CryptoStream cryptoStream = new CryptoStream(stream, dES.CreateDecryptor(), CryptoStreamMode.Write);
+            cryptoStream.Write(bytes, 0, bytes.Length);
+            cryptoStream.FlushFinalBlock();
+            return Encoding.Unicode.GetString(stream.ToArray());
+        }
+    }
+}
---
+++ Practicas/EncryptionPractice/EncryptionPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using CryptographyExample;
+using System;
+
+namespace EncryptionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "TEST TEST (AAAAA) SI 12345";
+            string password = "@eaaght";
+            Console.WriteLine($"{text}\n");
+            DESEncrypt encrypt = new DESEncrypt();
+            string encryptedText = encrypt.Encrypt(text, password);
+            Console.WriteLine($"TEXTO ENCRIPTADO: {encryptedText}\n");
+            string decryptedText = encrypt.Decrypt(encryptedText, password);
+            Console.WriteLine($"TEXTO DESENCRIPTADO: {decryptedText}\n");
+            Console.ReadLine();
+        }
+    }
+}
