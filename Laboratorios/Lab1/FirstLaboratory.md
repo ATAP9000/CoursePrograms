ba4a1382231ea93d5c36f837ab50464ab14daa7e
ATAP9000 <83297229+ATAP9000@users.noreply.github.com>
2022-11-28 07:30:40 -0400

    Add files via upload

---
+++ Laboratorios/Lab1/FirstLaboratory.md
@@ -0,0 +1,5 @@
+# Ejercicio 1
+
+1. Pedir por consola un nombre de persona y el nombre de una ciudad y mostrar por pantalla, el siguiente mensaje `«Hola » <nombre> » bienvenido a » <ciudad>`
+2. Pedir el nombre de la semana al usuario y decirle si es fin de semana o no.
+En caso de error, indicarlo.
---
+++ Laboratorios/Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su Nombre: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Ingrese el Nombre de una Ciudad : ");
+            string? city = Console.ReadLine();
+            Console.WriteLine($"Hola {name}, bienvenido a {city}");
+            Console.WriteLine("Porfavor ingrese el nombre de algun dia de la semana:");
+            string? dayName = Console.ReadLine();
+            switch (dayName?.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("Dia Laboral");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+            }
+        }
+    }
+}
